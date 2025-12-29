14d1ff46340b65d43f018a8d9c562ff18c9bfaa9
makamu-okinyi <makamubetsy@gmail.com>
2025-12-29 16:15:03 +0300

    Change health check enablement to false

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,7 +8,7 @@ const isDevServer = process.env.NODE_ENV !== "production";
 
 // Environment variable overrides
 const config = {
-  enableHealthCheck: process.env.ENABLE_HEALTH_CHECK === "true",
+  enableHealthCheck: process.env.ENABLE_HEALTH_CHECK === "false",
   enableVisualEdits: isDevServer, // Only enable during dev server
 };
 
